8666dba8c58b89631659654709ff24a2a8531976
pallavijyo <93638272+pallavijyo@users.noreply.github.com>
2021-11-18 16:57:39 +0530

    Update auth.js

--- Final-Project/lms/src/server/router/auth.js
+++ Final-Project/lms/src/server/router/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const authController = require('../controllers/authcontroller')
+const authController = require('..../controllers/authcontroller')
 
 //router.post('/register',authController.register); - if below didn't work, use this line of code instead.
 router.post('/register',authController.register.bind(authController.register))
